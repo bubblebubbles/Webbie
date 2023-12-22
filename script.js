515741f6c115a7266ab314d8f4ce3b98d2c04413
Nitesh Chaudhary <nitesh.chaudhary@finmechanics.com>
2023-12-22 17:21:30 +0530

    @WebDev @Adding css and js files

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('mousemove', function(e) {
+  const cursor = document.createElement('div');
+  cursor.classList.add('custom-cursor');
+  document.body.appendChild(cursor);
+
+  cursor.style.left = e.pageX - cursor.offsetWidth / 2 + 'px';
+  cursor.style.top = e.pageY - cursor.offsetHeight / 2 + 'px';
+
+  document.body.style.cursor = 'none';
+
+  document.addEventListener('mouseleave', function() {
+    document.body.removeChild(cursor);
+    document.body.style.cursor = 'auto';
+  });
+});
